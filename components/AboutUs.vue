1309294f1ec159546b6644cf648a81d83b4357f2
miltonmarma <miltonmarma120@gmail.com>
2023-07-31 23:31:46 +0600

    add image in static, assets folder for server deployment test

---
+++ components/AboutUs.vue
@@ -0,0 +1,13 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <div>this is about page.</div>
+    <img src="/lens.jpg" alt="lens picture" width="500" height="500"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutUs'
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <AboutUs/>
+</template>
+
+<script>
+import AboutUs from '~/components/AboutUs.vue'
+
+export default {
+  name: 'AboutPage',
+  components: { AboutUs }
+}
+</script>
