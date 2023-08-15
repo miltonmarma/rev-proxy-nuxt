1f8619d8d88d840e5673e3a3826b8f7a23d54309
miltonmarma <miltonmarma120@gmail.com>
2023-08-16 01:58:04 +0600

    add image, change text

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -2,6 +2,7 @@
 <template>
   <div>
     <div>this is about page.</div>
+    <h1>this is the main application.</h1>
     <img src="/lens.jpg" alt="lens picture" width="500" height="500"/>
   </div>
 </template>
